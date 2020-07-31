e50a60918f421cc2f8c24f3da6333a5fb4642a54
Reznik Denis <63472391+reznik-denis@users.noreply.github.com>
2020-07-31 19:02:20 +0300

    добавляет задачку 9

--- js/task-09.js
+++ js/task-09.js
@@ -27,12 +27,12 @@
 
 function isLoginValid(login, min = 4, max = 16) {
     // Write code under this line
-
+    return login.length >= min && login.length <= max
 }
 
 function isLoginUnique(allLogins, login) {
     // Write code under this line
-
+    return !allLogins.includes(login)
 }
 
 function addLogin(allLogins, login) {
@@ -41,19 +41,32 @@ function addLogin(allLogins, login) {
     const ERROR = 'Ошибка! Логин должен быть размером от 4 до 16 символов';
     let message;
     // Write code under this line    
+    isLoginValid(login);
+    if (!isLoginValid(login)) {
+        return message = ERROR;
+    }
 
+    isLoginUnique(allLogins, login);
+    if (!isLoginUnique(allLogins, login)) {
+        message = REFUSAL;
+        return message;
+    } else {
+        allLogins.push(login);
+        message = SUCCESS;
+    }
+    return message
 }
 
 const logins = ['Mango', 'robotGoogles', 'Poly', 'Aj4x1sBozz', 'qwerty123'];
 
-// console.log(addLogin(logins, 'Ajax')); 
+console.log(addLogin(logins, 'Ajax'));
 // 'Логин успешно добавлен!'
 
-// console.log(addLogin(logins, 'robotGoogles')); 
+console.log(addLogin(logins, 'robotGoogles'));
 // 'Такой логин уже используется!'
 
-// console.log(addLogin(logins, 'Zod'));
+console.log(addLogin(logins, 'Zod'));
 // 'Ошибка! Логин должен быть от 4 до 16 символов'
 
-// console.log(addLogin(logins, 'jqueryisextremelyfast')); 
-// 'Ошибка! Логин должен быть от 4 до 16 символов' 
+console.log(addLogin(logins, 'jqueryisextremelyfast'));
+// 'Ошибка! Логин должен быть от 4 до 16 символов'
